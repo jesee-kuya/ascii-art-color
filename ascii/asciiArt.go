83894a4a7bb6dfa42854cc990abf7cc696bed189
jkuya <kuyajesee@proton.me>
2024-05-16 16:10:19 +0300

    Refactored Ascii function and made it shorter

--- ascii/asciiArt.go
+++ ascii/asciiArt.go
@@ -14,14 +14,12 @@ func Ascii(fileArr []string, wordsArr []string, option []string, color string) {
 		if val != "" {
 			for i := 1; i <= 8; i++ {
 				for _, v := range val {
+					start := (v - 32) * 9
 					if len(opt) == 0 {
-						start := (v - 32) * 9
 						fmt.Print(color + fileArr[int(start)+i] + reset)
 					} else if strings.Contains(opt, string(v)) {
-						start := (v - 32) * 9
 						fmt.Print(color + fileArr[int(start)+i] + reset)
 					} else {
-						start := (v - 32) * 9
 						fmt.Print(fileArr[int(start)+i])
 					}
 				}
